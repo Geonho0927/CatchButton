ef81854abaa161aeacf04c31c7ed8e22a60d5f0e
Geonho0927 <moneljjang1234@naver.com>
2026-03-12 17:50:13 +0900

    게임 오버기능과 재시작기능

--- Form1.cs
+++ Form1.cs
@@ -6,13 +6,57 @@ namespace CatchButton
     public partial class Form1 : Form
     {
         int score = 0;
+        int outCount = 0;
         public Form1()
         {
             InitializeComponent();
         }
+        void RestartGame()
+        {
+            score = 0;
+            outCount = 0;
+
+            targetButton.Width = 250;
+            targetButton.Height = 120;
+
+            foreach (Control c in this.Controls)
+            {
+                if (c is Button)
+                {
+                    c.Enabled = true;
+                }
+            }
+
+            this.Text = "게임 다시 시작!";
+        }
 
         private void targetButton_MouseEnter(object sender, EventArgs e)
         {
+            
+            outCount ++;
+            if (outCount >= 20)
+            {
+                DialogResult result = MessageBox.Show(
+                "Game Over\n다시 시작하시겠습니까?",
+                "게임 종료",
+                MessageBoxButtons.YesNo
+                );
+                if (result == DialogResult.Yes) {
+                    RestartGame();
+                }
+                else
+                {
+                    foreach (Control c in this.Controls)
+                    {
+                        if (c is Button)
+                        {
+                            c.Enabled = false;
+                        }
+                    }
+                }
+                return;
+            }
+
             score -= 5;
             SystemSounds.Hand.Play();
 
@@ -26,7 +70,7 @@ namespace CatchButton
 
             targetButton.Location = new Point(nextX, nextY);
 
-            this.Text = $"버튼 위치: ({nextX}, {nextY}), 점수: {score}점";
+            this.Text = $"버튼 위치: ({nextX}, {nextY}), 점수: {score}점, 실패 횟수: {outCount}회";
         }
 
         private void targetButton_Click(object sender, EventArgs e)
