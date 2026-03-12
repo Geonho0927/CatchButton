70c77320abd13c42ab0efa39a2d2333ade0a87c7
Geonho0927 <moneljjang1234@naver.com>
2026-03-12 17:10:44 +0900

    점수와 난이도 조절 추가

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ namespace CatchButton
 {
     public partial class Form1 : Form
     {
+        int score = 0;
         public Form1()
         {
             InitializeComponent();
@@ -12,25 +13,28 @@ namespace CatchButton
 
         private void targetButton_MouseEnter(object sender, EventArgs e)
         {
-
+            score -= 5;
             SystemSounds.Hand.Play();
 
             Random rd = new Random();
 
-            int maxX = this.ClientSize.Width - 250;
-            int maxY = this.ClientSize.Height - 120;
+            int maxX = this.ClientSize.Width - targetButton.Width;
+            int maxY = this.ClientSize.Height - targetButton.Height;
 
             int nextX = rd.Next(0, maxX);
             int nextY = rd.Next(0, maxY);
 
             targetButton.Location = new Point(nextX, nextY);
 
-            this.Text = $"버튼 위치: ({nextX}, {nextY})";
+            this.Text = $"버튼 위치: ({nextX}, {nextY}), 점수: {score}점";
         }
 
         private void targetButton_Click(object sender, EventArgs e)
         {
+            score += 100;
             SystemSounds.Beep.Play();
+            targetButton.Width = (int)(targetButton.Width * 0.9);
+            targetButton.Height = (int)(targetButton.Height * 0.9);
             MessageBox.Show("축하합니다~!");
 
         }
